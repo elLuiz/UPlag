597c76ab8cda6e1b3dbe6bcbf888577581b3991d
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2021-07-08 18:25:49 -0300

    new tests, new regex added, new dependency

--- src/main/java/frontend/CFrontEnd.java
+++ src/main/java/frontend/CFrontEnd.java
@@ -1,16 +1,21 @@
 package frontend;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Getter
+@Setter
 public class CFrontEnd extends FrontEnd{
-    private static final String FUNCTION_REGEX = "(function\\s[a-zA-Z]+\\s[a-zA-Z0-9]+\\((.*)\\))\\s?\\{";
-    private static final String DOUBLE_SLASH_COMMENT_REGEX = "(\\/\\/[a-zA-ZÀ-Ùà-ù?*@.,!#$%¨&*\"\"''\\-()+{}^Ã-ã ]+\\s?)";
-    private static final String DOUBLE_ASTERISK_COMMENT_REGEX = "([\\/\\*\\*]|[\\d\\wÀ-Ùà-ùÁ-Ú?*@.,:!#$%¨&*;\"\"''\\-()+{}^Ã-ũ\\s?]+)";
-    private static final Logger LOGGER = Logger.getLogger(CFrontEnd.class.getName());
-
+    public static final String FUNCTION_REGEX = "[a-zA-Z]+(\\*?)+\\s(\\*?)[a-zA-Z]+\\s?\\(.*\\)\\s?\\{";
+    public static final String DOUBLE_SLASH_COMMENT_REGEX = "(\\/\\/[a-zA-ZÀ-Ùà-ùá-ú0-9?�Ã-ã\\p{Punct} ]+)";
+    public static final String DOUBLE_ASTERISK_COMMENT_REGEX = "((\\/\\*)(.*)(\\\\*\\/)$)";
+    public static final String FUNCTION_CALL_REGEX = "[a-zA-Z0-9]+(\\((.*)\\){1})\\;?";
+    public static final Logger LOGGER = Logger.getLogger(CFrontEnd.class.getName());
     @Override
     public void convertFunctionsOccurrencesToToken(String codeText) {
         Pattern pattern = Pattern.compile(FUNCTION_REGEX);
@@ -25,6 +30,5 @@ public class CFrontEnd extends FrontEnd{
 
     @Override
     public void convertVariableOccurrencesToToken(String codeText) {
-
     }
 }
--- src/test/java/com/br/uplag/CFrontEndTest.java
+++ src/test/java/com/br/uplag/CFrontEndTest.java
@@ -1,13 +1,59 @@
 package com.br.uplag;
 
+import frontend.CFrontEnd;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RunWith(JUnit4.class)
 public class CFrontEndTest {
+    private final String code = "/* Just a test */ /\n\n"
+            + "#include<stdio.h>\n"
+            + "#include<stdlib.h>\n"
+            + "#include<math.h>\n"
+            + "#include \"SkipList.h\"\n\n"
+            + "// Recebe uma skilist e o valor que deseja-se saber se pertence a ela.\n"
+            + "// Retorna 0 caso ela nao tenha sido alocada ou esteja vazia.\n"
+            + "// Retorna 1 caso o elemento esteja na lista.\n"
+            + "int buscaSkipList(skiplist *sk, int chave){\n\n"
+            + "    // Verifica se a lista foi alocada e se est vazia.\n"
+            + "	if(vaziaSkipList(sk)){\n"
+            + "        printf(\"Lista nao alocada ou vazia.\\n\");\n"
+            + "		return 0;\n"
+            + "}\n";
     @Test
-    public void shouldSayHello() {
-        System.out.println("hello");
+    public void shouldRemoveComments() {
+        final String regex = CFrontEnd.DOUBLE_ASTERISK_COMMENT_REGEX;
+        final String subst = "";
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(code);
+
+        final String result = matcher.replaceAll(subst);
+        Assert.assertEquals(false, result.contains("/**/"));
+    }
+
+    @Test
+    public void shouldRemoveDoubleSlashComments() {
+        final String slashRegex = CFrontEnd.DOUBLE_SLASH_COMMENT_REGEX;
+        final String substitute = "";
+        final Pattern pattern = Pattern.compile(slashRegex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(code);
+        final String result = matcher.replaceAll(substitute);
+        Assert.assertEquals(false, result.contains("//"));
+    }
+
+    @Test
+    public void shouldConvertFunctionOccurrencesToProperToken() {
+        final String functionRegex = CFrontEnd.FUNCTION_REGEX;
+        final String substitute = "FUN";
+        final Pattern pattern = Pattern.compile(functionRegex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(code);
+        final String result = matcher.replaceAll(substitute);
+        Assert.assertEquals(true, result.contains(substitute));
     }
 }
