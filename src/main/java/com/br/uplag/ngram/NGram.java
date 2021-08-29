25020b2430313c56b06f1e5e44dbc857e113d8cb
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2021-08-29 18:09:30 -0300

    moved packages

---
+++ src/main/java/com/br/uplag/ngram/NGram.java
@@ -0,0 +1,19 @@
+package com.br.uplag.ngram;
+
+public class NGram {
+    private Integer windowSize;
+
+    public NGram(Integer windowSize) {
+        this.windowSize = windowSize;
+    }
+
+
+    public String createNGrams(String content) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < content.length() - windowSize + 1; i++) {
+            stringBuilder.append(content, i, i + windowSize).append(" ");
+        }
+
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/com/br/uplag/ngram/NGramTest.java
@@ -0,0 +1,16 @@
+package com.br.uplag.ngram;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class NGramTest {
+    @Test
+    public void shouldCreateNGrams() {
+        NGram nGram = new NGram(4);
+        String content = "cFoAaDRp";
+        Assert.assertEquals(true, nGram.createNGrams(content).contains("cFoA FoAa oAaD AaDR aDRp"));
+    }
+}
