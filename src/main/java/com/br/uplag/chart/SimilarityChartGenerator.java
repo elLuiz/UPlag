9b95eee87d3585c04fcf8530e280c6568c73fdf2
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2021-09-17 18:02:27 -0300

    fixed otsu algorithm

--- src/main/java/com/br/uplag/chart/SimilarityChartGenerator.java
+++ src/main/java/com/br/uplag/chart/SimilarityChartGenerator.java
@@ -1,9 +1,12 @@
 package com.br.uplag.chart;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.statistics.HistogramDataset;
 
 import java.io.File;
@@ -22,12 +25,18 @@ public class SimilarityChartGenerator {
     public void createBarChart(double[] similarity) {
         HistogramDataset histogramDataset = createDataSet(similarity);
         JFreeChart jFreeChartHistogram = createHistogram(histogramDataset);
+        XYPlot xyPlot = (XYPlot) jFreeChartHistogram.getPlot();
+        NumberAxis numberAxis = (NumberAxis) xyPlot.getDomainAxis();
+        numberAxis.setRange(0.0, 100.0);
+        numberAxis.setTickUnit(new NumberTickUnit(10.0));
+        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
+        range.setTickUnit(new NumberTickUnit(1.0));
         shouldSaveChart(jFreeChartHistogram);
     }
 
     private HistogramDataset createDataSet(double[] similarity) {
         HistogramDataset histogramDataset = new HistogramDataset();
-        histogramDataset.addSeries("", similarity, 20);
+        histogramDataset.addSeries("", similarity, 10);
         return histogramDataset;
     }
 
@@ -38,7 +47,7 @@ public class SimilarityChartGenerator {
 
     private void shouldSaveChart(JFreeChart jFreeChartHistogram) {
         try {
-            ChartUtilities.saveChartAsPNG(new File(directory + "/histogram.png"), jFreeChartHistogram, 700, 500);
+            ChartUtils.saveChartAsPNG(new File(directory + "/histogram.png"), jFreeChartHistogram, 2500, 900);
         } catch (IOException ioException) {
             LOGGER.severe("Invalid directory");
             LOGGER.severe(ioException.getMessage());
