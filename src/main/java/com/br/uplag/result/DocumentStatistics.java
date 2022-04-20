697797edae761cf660aff60998899f28daa893fe
elLuiz <lu1zh3nr1qu34@gmail.com>
2022-04-20 19:25:25 -0300

    Added new tests, fixed system performance

--- src/main/java/com/br/uplag/result/DocumentStatistics.java
+++ src/main/java/com/br/uplag/result/DocumentStatistics.java
@@ -3,14 +3,13 @@ package com.br.uplag.result;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @Setter
 @Getter
 public class DocumentStatistics {
-    private Integer totalNumberOfTokens;
+    private int totalNumberOfTokens;
     private String[] nGrams;
     private Double containment;
 
@@ -18,12 +17,28 @@ public class DocumentStatistics {
         this.totalNumberOfTokens = nGrams.split(" ").length;
     }
 
-    public void calculateContainment(DocumentStatistics documentStatisticsB) {
-        Set<String> originalSet = new HashSet<>(Arrays.asList(getNGrams()));
-        Set<String> documentANgrams = new HashSet<>(Arrays.asList(getNGrams()));
-        Set<String> documentBNgrams = new HashSet<>(Arrays.asList(documentStatisticsB.getNGrams()));
+    public void calculateContainment(DocumentStatistics secondDocumentStatistics) {
+        Set<String> documentANgrams = getNGramsSet(this.getNGrams());
+        Set<String> documentBNgrams = getNGramsSet(secondDocumentStatistics.getNGrams());
+        intersectCommonNgrams(documentANgrams, documentBNgrams);
+        this.containment = ((double) documentANgrams.size() / getNgramsSize());
+    }
+
+    private Set<String> getNGramsSet(String[] ngrams) {
+        Set<String> ngramsSet = new HashSet<>();
+        for (String ngram : ngrams) {
+            ngramsSet.add(ngram);
+        }
+
+        return ngramsSet;
+    }
+
+    private void intersectCommonNgrams(Set<String> documentANgrams, Set<String> documentBNgrams) {
         documentANgrams.retainAll(documentBNgrams);
-        this.containment = ((double) documentANgrams.size() / originalSet.size());
+    }
+
+    private int getNgramsSize() {
+        return this.getNGrams().length;
     }
 
     @Override
